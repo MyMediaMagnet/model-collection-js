6b6f398cb70f0c28e41ee806ddb045f6bb45fbe0
Troy Jaggard <troy@mymediamagnet.com>
2018-03-13 20:42:26 -0600

    get actual code running in tests

---
+++ src/index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+import Collection from './classes/Collection'
+import Query from './classes/Query'
+import Model from './models/Model'
--- test/test.js
+++ test/test.js
@@ -1,47 +1,18 @@
 'use strict';
 
 var expect = require('chai').expect;
-var numFormatter = require('../index');
+let Collection = require('../dist/classes/Collection').default
 
-describe('#numFormatter', function() {
-    it('should convert single digits', function() {
-        var result = numFormatter(1);
-        expect(result).to.equal('1');
+describe('#Collection', function() {
+    it('it should grab the first item in the array', function() {
+        let array = [1, 2, 3, 4]
+        let collection = new Collection(array)
+        expect(collection.first()).to.equal(1);
     });
 
-    it('should convert double digits', function() {
-        var result = numFormatter(12);
-        expect(result).to.equal('12');
+    it('it should grab the last item in the array', function() {
+        let array = [1, 2, 3, 4]
+        let collection = new Collection(array)
+        expect(collection.last()).to.equal(4);
     });
-
-    it('should convert triple digits', function() {
-        var result = numFormatter(123);
-        expect(result).to.equal('123');
-    });
-
-    it('should convert 4 digits', function() {
-        var result = numFormatter(1234);
-        expect(result).to.equal('1,234');
-    });
-
-    it('should convert 5 digits', function() {
-        var result = numFormatter(12345);
-        expect(result).to.equal('12,345');
-    });
-
-    it('should convert 6 digits', function() {
-        var result = numFormatter(123456);
-        expect(result).to.equal('123,456');
-    });
-
-    it('should convert 7 digits', function() {
-        var result = numFormatter(1234567);
-        expect(result).to.equal('1,234,567');
-    });
-
-    it('should convert 8 digits', function() {
-        var result = numFormatter(12345678);
-        expect(result).to.equal('12,345,678');
-    });
-    
 });
