6bab2238dd0409f368943a6b904147d094795857
Troy Jaggard <troy@mymediamagnet.com>
2018-03-14 01:04:42 -0600

    extend tests and make fails pass

--- test/model.js
+++ test/model.js
@@ -4,11 +4,20 @@ var expect = require('chai').expect;
 let Model = require('../dist/models/Model').default
 
 describe('#Model', function() {
+
     it('it should collect an array of User models', function() {
         let array = [{id: 1, name: 'Jack', email: 'jack@jones.ca'}, {id: 1, name: 'Stacy', email: 'stacy@jones.ca'}]
         let users = User.collect(array)
         expect(users.first().email).to.equal('jack@jones.ca');
         expect(users.first()).to.instanceof(User);
+    });
+
+    it('it should be able to create a User from an object or array', function() {
+        let object = {id: 1, name: 'Jack', email: 'jack@jones.ca'}
+        let user = new User()
+        user.set(object)
+        expect(user.email).to.equal('jack@jones.ca');
+        expect(user).to.instanceof(User);
     });
 });
 
